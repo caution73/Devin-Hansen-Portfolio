247e268840bde1661200dbf784b1bfcdc44046c2
Devin Hansen <hansdevi@isu.edu>
2023-05-13 17:40:07 -0500

    Final commit for the day.

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,6 +3,8 @@ export default function About() {
         <div className="about">
             <h4>Here's a bit about me</h4>
 
+            <img src="https://placekitten.com/200/300" width="200" height="300"/>
+
             <p>My name is Devin Hansen. I come from an extensive background in sales, customer service, creative/performing arts, and most recently software engineering. I have 14 years of experience in sales and customer service via positions including Enterprise Rental Car and Vector Marketing, where I was regularly recognized for being a top 10 performer in generating additional revenue as well as providing quality customer service. I have over 20 years of team experience, including leadership roles, in those positions as well as choral and instrumental ensembles that I have been a part of. Most recently, I have been participating in a rigorous software engineering training offered through Per Scholas, where I completed dozens of coding projects using computer programming languages/methods such as HTML, CSS, Javascript, React, etc.  
 </p>
         </div>
