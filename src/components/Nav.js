81135eb6e3268827c111eb92aabf4e9b2d496e87
Devin Hansen <hansdevi@isu.edu>
2023-05-12 22:39:56 -0500

    Final commit for the night.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom"
+
 export default function Nav() {
+    const links = ["My work", "My skills", "My life"]
     return(
         <div className="nav">
-            <h2>Nav</h2>
+            {links.map((link, i) => {
+                return(
+                    <Link to={`/${link}`}>
+                        <div>
+                            <h4>{link}</h4>
+                        </div>
+                    </Link>
+                )
+            })}
         </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import Carousel from "../components/Carousel";
 export default function Home(){
     return(
         <div id="homePage">
-            <h1>Prized Possessions</h1>
+            <h1>Highlights</h1>
             <Carousel />
         </div>
     )
