0269c56de894ec3de2ec154f47a140ff0b76791e
Devin Hansen <hansdevi@isu.edu>
2023-05-17 22:36:20 -0500

    Trying to add content to webApps page.

---
+++ src/assets/MyWorks.js
@@ -0,0 +1,39 @@
+const MyWorks = [
+  {
+    webApps: [
+      {
+        title: "RoadTripCentral Website",
+        alt: "RoadTripCentral's home page.",
+        article: "",
+        tech: "React.js, Javascript, HTML, CSS, React router",
+        url: "RoadTripCentralHome--Screenshot.PNG",
+        deployLink: "https://caution73-roadtripcentral-site.netlify.app/",
+        repoLink: "",
+      },
+      {
+        title: "Git-a-Bob",
+        alt: "A choose-your-own-adventure game.",
+        article: "",
+        tech: "HTML, CSS, Javascript, DOM",
+        url: "Adventure-Game---Screenshot-1.PNG",
+        deployLink: "https://caution73.github.io/mod1_story_project/",
+        repoLink: "",
+      }
+    ],
+  },
+  {
+    algorithms: [
+      {
+        title: "Luhn Algorithm",
+        alt: "Three variations on the Luhn Algorithm",
+        article: "",
+        tech: "Javascript, using loops and conditionals.",
+        url: "luhn-algo-screenshot.PNG",
+        repoLink:
+          "https://codesandbox.io/s/luhn-algorithm-dhansen-forked-6kvuvk",
+      },
+    ],
+  },
+];
+
+module.exports = MyWorks;
--- src/pages/WebApps.js
+++ src/pages/WebApps.js
@@ -1,7 +1,18 @@
+import { webApps } from "../assets/MyWorks"
+
 export default function WebApps() {
+    const apps = webApps
+    console.log(webApps)
     return(
         <div>
             <h4>Here's the webapps page</h4>
+            {/* {apps.map((webApp) => {
+                return(
+                    <div>
+                        {webApp.title}
+                    </div>
+                )
+            })} */}
         </div>
     )
 }
